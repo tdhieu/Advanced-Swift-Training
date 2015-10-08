9bd4b2415768d76545598c5ff9eec91df5b0e290
Trần Đức Hiếu <tranduchieu@gmail.com>
2015-10-08 18:43:40 +0700

    Submit on Oct 8th 2015

---
+++ AdvCollectionView/AdvCollectionView/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  AdvCollectionView
+//
+//  Created by Tran Duc Hieu on 10/8/15.
+//  Copyright (c) 2015 Duy-Hieu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var myImage: UIImageView!
+    
+    @IBOutlet var lblName: UILabel!
+
+    @IBOutlet var lblPrice: UILabel!
+    
+    @IBOutlet var blurView: UIVisualEffectView!
+}
---
+++ AdvCollectionView/AdvCollectionView/CollectionViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CollectionViewController.swift
+//  AdvCollectionView
+//
+//  Created by Tran Duc Hieu on 10/8/15.
+//  Copyright (c) 2015 Duy-Hieu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+
+    var arrImage:[String] = ["dog1.png", "dog2.png","dog3.png","dog4.png","dog5.png","dog6.png","dog7.png","dog8.png"]
+    
+    var arrName:[String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+    
+    var arrPrice:[Int] = [1000000, 1500000, 360000,500000, 706000, 800000, 9000000, 3100000]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return arrImage.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! CollectionViewCell
+        
+        cell.myImage.image = UIImage(named: arrImage[indexPath.row])
+        cell.lblName.text = arrName[indexPath.row]
+        cell.lblPrice.text = String(arrPrice[indexPath.row])
+        cell.blurView.layer.cornerRadius = 10.0
+        cell.blurView.clipsToBounds = true
+        
+        
+        return cell
+    }
+
+}
