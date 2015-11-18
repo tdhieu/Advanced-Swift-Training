84c867a3ad12cdfc75de9a97a2d8a8eb7bfe2f1e
Hieu Tran <tranduchieu@gmail.com>
2015-11-18 16:38:55 +0700

    Change names of SpriteKit projects

---
+++ SpriteKit_Game02/SpriteKit_Game02/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  SpriteKit_Game02
+//
+//  Created by Tran Duc Hieu on 11/18/15.
+//  Copyright (c) 2015 Tran Duc Hieu. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+    }
+    
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ SpriteKit_Game02/SpriteKit_Game02Tests/SpriteKit_Game02Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SpriteKit_Game02Tests.swift
+//  SpriteKit_Game02Tests
+//
+//  Created by Tran Duc Hieu on 11/18/15.
+//  Copyright © 2015 Tran Duc Hieu. All rights reserved.
+//
+
+import XCTest
+@testable import SpriteKit_Game02
+
+class SpriteKit_Game02Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
