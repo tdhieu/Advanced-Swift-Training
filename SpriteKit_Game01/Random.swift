5a0a7d45708a2048c0b4e878b5fb0a15b708662a
Hieu Tran <tranduchieu@gmail.com>
2015-11-18 16:30:22 +0700

    Finish SpriteKit Game01

---
+++ SpriteKit_Game01/Random.swift
@@ -0,0 +1,18 @@
+//
+//  Random.swift
+//  SpriteKit_Game01
+//
+//  Created by Tran Duc Hieu on 11/18/15.
+//  Copyright © 2015 Tran Duc Hieu. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+func random(min: CGFloat, max: CGFloat) -> CGFloat {
+    return CGFloat(arc4random_uniform(UInt32(max-min+1))) + min
+}
+
+func random(min: Int, max: Int) -> Int {
+    return Int(arc4random_uniform(UInt32(max-min+1))) + min
+}
--- SpriteKit_Game01/SpriteKit_Game01/GameScene.swift
+++ SpriteKit_Game01/SpriteKit_Game01/GameScene.swift
@@ -12,7 +12,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // ------------------ Khai bao bien -------------------//
     var nodeGunship: SKSpriteNode = SKSpriteNode(imageNamed: "gunship.png")  // sung ban quai vat
-    var nodeMonster: SKSpriteNode = SKSpriteNode(imageNamed: "monster1.png")  // quai vat
+    var nodeMonster: SKSpriteNode!
     var arrBullet:[SKSpriteNode] = []
     var nodeBullet: SKSpriteNode!
     var moved: Bool = false
@@ -38,18 +38,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         nodeGunship.physicsBody?.affectedByGravity = false
         nodeGunship.name = "Gunship"
         nodeGunship.physicsBody?.categoryBitMask = vatthe.nodeGunship.rawValue
-
-        // ------------------- Node Monster --------------------//
-        nodeMonster.position = CGPoint(x: 200, y: 500)
-        nodeMonster.size = CGSize(width: 100, height: 100)
-        nodeMonster.physicsBody = SKPhysicsBody(texture: nodeMonster.texture!, size: nodeMonster.size)
-        nodeMonster.physicsBody?.dynamic = true
-        nodeMonster.physicsBody?.affectedByGravity = false
-        nodeMonster.name = "Monster"
-        nodeMonster.physicsBody?.categoryBitMask = vatthe.nodeMonster.rawValue
-        nodeMonster.physicsBody?.contactTestBitMask = vatthe.nodeBullet.rawValue
-        self.addChild(nodeMonster)
-
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -73,19 +61,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             let actionBySequence = SKAction.sequence([moveToPosition, removeNode])
 
             nodeBullet.runAction(actionBySequence)
-            // bullet sau khi lam xong action 1 se bi xoa di
-//            nodeBullet.runAction(moveToPosition, completion: {
-//                self.nodeBullet.runAction(removeNode)
-//                
-//            })
+            
         }
 
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-//        var nodeBullet:SKSpriteNode = arrBullet[0]
-//        nodeBullet.removeFromParent()
-//        arrBullet.removeAtIndex(0)
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -96,13 +77,49 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
-        // Neu doi tuong va cham la monster thi ta remove monster ra khoi parent
+        // Neu doi tuong va cham la monster thi ta remove monster va bullet ra khoi parent
         if contact.bodyA.categoryBitMask == vatthe.nodeMonster.rawValue || contact.bodyB.categoryBitMask == vatthe.nodeMonster.rawValue {
             nodeMonster.removeFromParent()
+            nodeBullet.removeFromParent()
         }
+    }
+    
+    var timerCreateMonster:CFTimeInterval = 0
+    
+    func CreateMonster() {
+        
+        // ------------------- Node Monster --------------------//
+        let number = random(1, max: 7)
+        nodeMonster = SKSpriteNode(imageNamed: "monster\(number).png")  // quai vat
+        nodeMonster.position = CGPoint(x: random(self.size.width*0.3, max: self.size.width*0.7), y: self.size.height)
+        nodeMonster.size = CGSize(width: 50, height: 50)
+        nodeMonster.physicsBody = SKPhysicsBody(texture: nodeMonster.texture!, size: nodeMonster.size)
+        nodeMonster.physicsBody?.dynamic = true
+        nodeMonster.physicsBody?.affectedByGravity = false
+        nodeMonster.name = "Monster"
+        nodeMonster.physicsBody?.categoryBitMask = vatthe.nodeMonster.rawValue
+        nodeMonster.physicsBody?.contactTestBitMask = vatthe.nodeBullet.rawValue
+        self.addChild(nodeMonster)
+        
+        let moveDown:SKAction = SKAction.moveToY(0, duration: 2)
+        let removeNode:SKAction = SKAction.removeFromParent()
+//        let actionBySequence = SKAction.sequence([moveDown, removeNode])
+//        
+//        nodeMonster.runAction(actionBySequence)
+        nodeMonster.runAction(moveDown)
+        // monster sau khi di chuyen xong se bi xoa di
+        nodeMonster.runAction(moveDown, completion: {
+            self.nodeMonster.runAction(removeNode)
+        })
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        print(currentTime)
+        timerCreateMonster += currentTime
+        if timerCreateMonster >= 100000 {
+            CreateMonster()
+            timerCreateMonster = 0
+        }
     }
 }
