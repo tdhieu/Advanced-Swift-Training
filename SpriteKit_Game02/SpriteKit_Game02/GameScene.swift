7e1e5c85c77b3064ecdf7ee1d477001194a7011a
Hieu Tran <tranduchieu@gmail.com>
2015-11-18 16:54:04 +0700

    SpriteKit Game 02

--- SpriteKit_Game02/SpriteKit_Game02/GameScene.swift
+++ SpriteKit_Game02/SpriteKit_Game02/GameScene.swift
@@ -9,7 +9,23 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var nodeTank:SKSpriteNode!
+    var nodeMonster:SKSpriteNode!
+    
+    enum nodeList:UInt32 {
+        case nodeTank = 1
+        case nodeMonster = 2
+    }
     override func didMoveToView(view: SKView) {
+        nodeTank = SKSpriteNode(imageNamed: "tank.png")
+        nodeTank.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        nodeTank.size = CGSize(width: 100, height: 100)
+        nodeTank.physicsBody?.dynamic = true
+        nodeTank.physicsBody?.affectedByGravity = false
+        nodeTank.physicsBody?.categoryBitMask = nodeList.nodeTank.rawValue
+        nodeTank.physicsBody?.contactTestBitMask = nodeList.nodeMonster.rawValue
+        nodeTank.name = "tank"
+        self.addChild(nodeTank)
     }
     
    
