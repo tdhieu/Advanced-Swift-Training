2472895fee9b1c5b232cf059173a865a0573b5ed
Hieu Tran <tranduchieu@gmail.com>
2015-12-13 21:44:00 +0700

    Adjust the Auto Layout Constraints

---
+++ StrutsProblem/StrutsProblem/AutoLayoutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AutoLayoutViewController.swift
+//  StrutsProblem
+//
+//  Created by Tran Duc Hieu on 12/13/15.
+//  Copyright © 2015 Tran Duc Hieu. All rights reserved.
+//
+
+import UIKit
+
+class AutoLayoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
